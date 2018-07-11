9d70bb3576fbcf11419ff403c72366c7ecdd8d2d
carlfvarela <carlfvarela@gmail.com>
2018-07-11 16:13:16 +0100

    add pilot; show as dict; v2

--- basic_class.py
+++ basic_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: ts=4 sts=4 sw=4 si et
+import sys
+import random
+import os
 
 
 class Event:
+
     def __init__(self, date, location, num_pilots_n):
 
         self.date = date
@@ -18,71 +22,63 @@ class Event:
         date = input('Date: ')
 
         location = input('Choose spot: \n \
-                    Aqua (1)\n \
-                    Famões (2)\n\
-                    Expo (3)\n ')
-        num_pilots_n = input('Choose number of pilots: \n \
-                1 \n \
-                2 \n \
-                3 \n \
-                4 \n ')
+                    teste1 (1)\n \
+                    teste2 (2)\n\
+                    teste3 (3)\n ')
+        num_pilots_n = input('Choose number of pilots: ')
+
         try:
             location in range(1, 3)
-
             if location == 1:
-                location = "Aqua"
+                location = "teste1"
             elif location == 2:
-                location = "Famões"
+                location = "teste2"
             elif location == 3:
-                location == "Expo"
+                location == "teste3"
             else:
                 location = None
                 raise Exception
         except Exception:
             print("deu merda")
-
         finally:
             return cls(date, location, int(num_pilots_n))
 
     def addPilot(self, num_pilots_n):
-        # i = 0
-        # for i in range(0, num_pilots_n):
-        #     i = i + 1
-        #     pilots = []
-        #     #print (num_pilots_n)
-        #     pilots.append(Pilot(self, Pilot))
-
-        # print (pilots)
-        # print (i)
-        first = input("First name: ")
-        last = input("last name: ")
-        print(type(num_pilots_n))
-        i = 0
-        pilots = []
+        i = 1
+        pilot_list = []
         while i <= num_pilots_n:
-            pilot = Pilot(first, last)
-            pilots.append(pilot)
+            print("Pilot nº %s" % i)
+            print("Enter first name")
+            first = sys.stdin.readline()
+            print("ĺast")
+            last = sys.stdin.readline()
+            print("---------------------------")
+            clear()
+            num = i
+
+            pilot_list.append(Pilot(num, first, last))
+
+            for n in pilot_list:
+                print (n.__dict__['first'] + n.__dict__['last'])
+
+            #pilots = dict(zip(pilot_list))
+
             i += 1
+        #print (pilot_list)
 
-        print(pilot)
-
-    # def registerPilot(self, num_pilots_n):
-    #     i = 0
-    #     for i in range(1, num_pilots_n):
-    #         i = i + 1
-    #         return Pilot.addPilot()
+    def clear():
+        os.system('cls')
 
 
 class Pilot:
-    def __init__(self, first, last):
+
+    def __init__(self, num, first, last):
+        self.num = int(num)
         self.first = first
         self.last = last
 
-    @classmethod
-    def pilot_input(clspilot):
-        first = input("First name: ")
-        last = input("last name: ")
-        return cls(first, last)
+    def __str__(self):
+        return '{} - {} {}'.format(self.num, self.first, self.last)
 
 
 event1 = Event.from_input()
--- proj_test/test_2.py
+++ proj_test/test_2.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: ts=4 sts=4 sw=4 si et
-
 import sys
+import random
 
 
 class Event:
+
     def __init__(self, date, location, num_pilots_n):
 
         self.date = date
         self.location = location
-        self.num_pilots_n = num_pilots_n
+        self.num_pilots_n = int(num_pilots_n)
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
@@ -19,61 +20,60 @@ class Event:
     def from_input(cls):
         date = input('Date: ')
 
-        location = str(input('Choose spot: \n \
-                    test1 (1)\n \
-                    test2 (2)\n\
-                    test3 (3)\n '))
+        location = input('Choose spot: \n \
+                    teste1 (1)\n \
+                    teste2 (2)\n\
+                    teste3 (3)\n ')
         num_pilots_n = input('Choose number of pilots: \n \
                 1 \n \
                 2 \n \
                 3 \n \
                 4 \n ')
+
         try:
             location in range(1, 3)
-
             if location == 1:
-                location = "test1"
+                location = "teste1"
             elif location == 2:
-                location = "test2"
+                location = "teste2"
             elif location == 3:
-                location == "test3"
+                location == "teste3"
             else:
                 location = None
                 raise Exception
         except Exception:
             print("deu merda")
-
         finally:
-            return cls(date, location, num_pilots_n)
+            return cls(date, location, int(num_pilots_n))
 
-    def addPilot():
-        first = input("First name: ")
-        last = input("last name: ")
-        return Pilot.newPilot(first, last)
+    def addPilot(self, num_pilots_n):
+        i = 1
+        pilot_list = []
+        while i <= num_pilots_n:
+            print("Pilot nº %s" % i)
+            print("Enter first name")
+            first = sys.stdin.readline()
+            print("ĺast")
+            last = sys.stdin.readline()
+            num = random.randint(1, 50)
+            #Pilot(num, first, last)
+            # pilots.append(pilot.first)
+
+            pilot_list.append(Pilot(num, first, last))
+            print (pilot_list[0].__dict__)
+
+            #pilots = dict(zip(pilot_list))
+
+            i += 1
+        print (pilot_list)
 
 
 class Pilot:
-    def __init__(self, first, last):
+
+    def __init__(self, num, first, last):
+        self.num = int(num)
         self.first = first
         self.last = last
-
-    # @classmethod
-    # def pilot_input(clspilot):
-    #     first = input("First name: ")
-    #     last = input("last name: ")
-    #     return cls(first, last)
-
-    def newPilot():
-
-        #print(num_pilots_n)
-        i = 0
-        pilots = []
-        while i <= num_pilots_n:
-            print(num_pilots_n)
-            pilot = Pilot(first, last)
-            pilots.append(pilot)
-            i += 1
-            print(pilot)
 
 
 event1 = Event.from_input()
@@ -81,6 +81,5 @@ print("---------------------------")
 print(event1.location)
 print("---------------------------")
 print("---------------------------")
-
-event1.addPilot
-print()
+event1.addPilot(event1.num_pilots_n)
+print(event1)
